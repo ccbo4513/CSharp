74859953f33b81cac170703d623ed204575b418f
传波 陈 <arvon_chen@163.com>
2020-11-06 13:46:33 +0800

    sizeof运用

---
+++ demo1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace chencb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            float b = 2;
+            Console.WriteLine(sizeof(double));
+            Console.ReadKey();
+        }
+    }
+}
